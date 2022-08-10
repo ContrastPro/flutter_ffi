2800c3b41e484ef10df7b0108027b194e47b87af
Hlib Krasnov <gleb.k@redwerk.com>
2022-08-10 12:19:39 +0300

    Added support for IOS

--- packages/adder/ios/Classes/SwiftAdderPlugin.swift
+++ packages/adder/ios/Classes/SwiftAdderPlugin.swift
@@ -11,4 +11,9 @@ public class SwiftAdderPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+
+  public func dummyMethodToEnforceBundling() {
+    // call some function from our static lib
+    add(40, 2)
+  }
 }
